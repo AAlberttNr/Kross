37e1e83e1bff91c0bd244bee46fc87b63d9340a9
Albert <testingpon@gmail.com>
2023-12-27 18:01:13 +0600

    Albert

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -2,11 +2,25 @@ import React from 'react'
 import css from './Hero.module.scss'
 
 const Hero = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+	return (
+		<div className='container'>
+			<div className={css.hero}>
+				<div className={css.hero__left}>
+					<h1>
+						Adidas <br /> Nite Jogger
+					</h1>
+					<p>Городские кроссовки в<br/> ярком стиле 80-х</p>
+					<button
+						className={css.hero__left_button}
+					>
+						Смотреть все
+					</button>
+				</div>
+				<div></div>
+			</div>
+			<div></div>
+		</div>
+	)
 }
 
 export default Hero
